6062e552e512a46a2d499acd97ff07c11eb5697a
Zsolty11 <73882422+Zsolty11@users.noreply.github.com>
2022-01-18 17:05:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+Nagyobb szám.
+A program kérjen be két számot a felhasználótól, majd írja ki, hogy melyik a nagyobb! 
+A program üzeneteinek megfogalmazásában kövesse az alábbi mintát:
+--------------
+Adj meg egy számot! 1
+Adj meg egy másik számot! 17
+A nagyobb érték 17
+--------------
+Adj meg egy számot! 28
+Adj meg egy másik számot! -2
+A nagyobb érték: 28
+---------------
+Adj meg egy számot! 999
+Adj meg egy másik számot! 999
+A két szám egyenlő
+------------------
+'''
+
+
+szam1 = input("Adj meg egy számot!")
+szam2 = input("Adj meg egy másik számot!")
+
+if szam1 < szam2 :
+  print(f"A nagyobb érték:{szam2}")
+elif szam1 > szam2:
+  print(f"A nagyobb érték:{szam1}")
+else:
+  print("A két szám egyenlő")
+  
